897eb7a41c4bb744d1da31456382f93f9f4bae04
GavinPinto <gavinpinto620@gmail.com>
2025-02-07 19:23:15 -0500

    added view all button + more styling

--- Bike-Computer/App/app/src/main/java/com/example/app/RPDevicesSection.kt
+++ Bike-Computer/App/app/src/main/java/com/example/app/RPDevicesSection.kt
@@ -1,56 +1,63 @@
 package com.example.app
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-
-@Composable
-fun RPDevicesSection() {
-    Column {
-        Title()
-
-    }
-}
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 private fun Title() {
     Text(
         text = "Recently Paired Devices:",
-        modifier = Modifier.padding(bottom = 8.dp)
+        fontSize = 24.sp,
+        fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+        modifier = Modifier.padding( start = 8.dp, bottom = 8.dp)
     )
 }
 
 @Composable
-fun DisplayDevices(devicesList: List<String>) {
-    Column(verticalArrangement = Arrangement.spacedBy((-8).dp)) {
-        devicesList.forEach { device ->
+private fun DisplayDevices(devicesList: List<String>, amount: Int = devicesList.size) {
+    Column(verticalArrangement = Arrangement.spacedBy(3.dp)) {
+        for (i in 0..< amount) {
             Button(
                 onClick = { /*TODO*/ },
+                elevation = ButtonDefaults.buttonElevation(pressedElevation = 3.dp),
                 shape = RectangleShape,
                 colors = ButtonDefaults.buttonColors(containerColor = Color.White),
-                border = ButtonDefaults.outlinedButtonBorder,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .border(1.dp, Color.Black)
+                    .height(60.dp)
+
             ) {
                 Text(
-                    text = device,
-                    color = Color.Black
+                    text = devicesList[i],
+                    color = Color.Black,
+                    textAlign = TextAlign.Left,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .wrapContentHeight(align = Alignment.CenterVertically),
+                    fontSize = 20.sp,
+                    fontFamily = androidx.compose.ui.text.font.FontFamily.Monospace
                 )
             }
         }
@@ -58,10 +65,39 @@ fun DisplayDevices(devicesList: List<String>) {
 }
 
 @Composable
-fun RPDevices(devicesList: List<String>) {
-    Column {
+private fun ViewAllButton() {
+    Box(Modifier.fillMaxWidth()) {
+        Button(
+            colors = ButtonDefaults.buttonColors(containerColor = Color(red = 50, green = 200, blue = 50)),
+            modifier = Modifier
+                .padding(10.dp)
+                .height(60.dp)
+                .width(180.dp)
+                .align(Alignment.Center)
+                .wrapContentHeight(Alignment.CenterVertically),
+            onClick = { /*TODO*/ },
+        ) {
+            Text(
+                text = "View All",
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .wrapContentHeight(align = Alignment.CenterVertically),
+                fontSize = 24.sp,
+            )
+        }
+    }
+
+}
+
+@Composable
+fun RPDevices(devicesList: List<String>, modifier: Modifier = Modifier, amount: Int = devicesList.size) {
+    Column(modifier = modifier) {
         Title()
-        DisplayDevices(devicesList)
+        DisplayDevices(devicesList, amount)
+        ViewAllButton()
     }
 }
 
@@ -79,7 +115,9 @@ fun RPDevicesPreview() {
             "Someone's Bose Headphones",
             "Computer",
             "Smart Fridge"
-        )
+        ),
+        amount = 5,
+        modifier = Modifier.padding(16.dp)
     )
 }
 
